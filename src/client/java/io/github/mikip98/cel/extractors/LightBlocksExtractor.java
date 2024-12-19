b10c410c6fe7226a12058693c099f6bb3f5f1a07
MikiP98 <pokora.mikolaj@gmail.com>
2024-12-20 00:05:42 +0100

    Light emitting blocks extractor update

--- src/client/java/io/github/mikip98/cel/extractors/LightBlocksExtractor.java
+++ src/client/java/io/github/mikip98/cel/extractors/LightBlocksExtractor.java
@@ -3,46 +3,58 @@ package io.github.mikip98.cel.extractors;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.registry.Registries;
-import net.minecraft.state.property.Property;
+import net.minecraft.state.property.*;
+import net.minecraft.util.math.Direction;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static io.github.mikip98.cel.ColorExtractorLibraryClient.LOGGER;
 
 public class LightBlocksExtractor {
+    @SuppressWarnings({"unchecked", "rawtypes"})
     public static void getLightEmittingBlockstates() {
         for (Block block : Registries.BLOCK) {
             BlockState blockState = block.getDefaultState();
 
-            Collection<Property<?>> properties = blockState.getProperties();
-
-
-
-//            // Assuming blockState has a method to get its properties
-//            Collection<Property<?>> properties = blockState.getProperties();
+            Collection<Property<? extends Comparable<?>>> properties = blockState.getProperties();
 
             // Generate all combinations of property values
             List<Map<Property<?>, Comparable<?>>> combinations = generateCombinations(properties);
 
             // Test each combination
             for (Map<Property<?>, Comparable<?>> combination : combinations) {
-                BlockState testState = blockState;  // .withProperties(combination);
+                BlockState testState = blockState;
+
                 for (Map.Entry<Property<?>, Comparable<?>> entry : combination.entrySet()) {
-
                     Property<?> property = entry.getKey();
-                    @SuppressWarnings("rawtypes")
-                    Comparable value = entry.getValue();
 
-                    // Apply the property-value pair to the block state
-                    testState = testState.with(property, value);
+                    if (property instanceof BooleanProperty new_property) {
+                        testState = testState.with(new_property, (Boolean) entry.getValue());
+
+                    } else if (property instanceof DirectionProperty new_property) {
+                        testState = testState.with(new_property, (Direction) entry.getValue());
+
+                    } else if (property instanceof EnumProperty new_property) {
+                        testState = testState.with(new_property, (Enum) entry.getValue());
+
+                    } else if (property instanceof IntProperty new_property) {
+                        testState = testState.with(new_property, (Integer) entry.getValue());
+
+                    } else {
+                        LOGGER.warn("Unsupported property type: {}", property.getClass());
+                    }
                 }
+
                 if (testState.getLuminance() > 0) {
-                    LOGGER.info("Luminance > 0 for combination: {}", combination);
+                    LOGGER.info("Luminance `{}` for combination: {}", testState.getLuminance(), combination);
                 }
             }
         }
     }
-
 
 
 
@@ -53,8 +65,10 @@ public class LightBlocksExtractor {
     }
 
     private static void generateCombinationsRecursive(
-            List<Property<?>> properties, Map<Property<?>, Comparable<?>> currentCombination,
-            List<Map<Property<?>, Comparable<?>>> result, int index
+            List<Property<?>> properties,
+            Map<Property<?>, Comparable<?>> currentCombination,
+            List<Map<Property<?>, Comparable<?>>> result,
+            int index
     ) {
         if (index == properties.size()) {
             result.add(new HashMap<>(currentCombination));
