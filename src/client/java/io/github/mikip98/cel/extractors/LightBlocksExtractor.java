38d8012c0fd4f571f75920a5c3c5a64bda0a9626
MikiP98 <pokora.mikolaj@gmail.com>
2024-12-21 21:02:05 +0100

    Type improvements

--- src/client/java/io/github/mikip98/cel/extractors/LightBlocksExtractor.java
+++ src/client/java/io/github/mikip98/cel/extractors/LightBlocksExtractor.java
@@ -11,23 +11,12 @@ import static io.github.mikip98.cel.ColorExtractorLibraryClient.LOGGER;
 
 public class LightBlocksExtractor {
     @SuppressWarnings({"unchecked", "rawtypes"})
-    public static HashMap<String, HashMap<String, HashMap<Byte, HashSet<HashMap<Property<? extends Comparable<?>>, Comparable<?>>>>>> getLightEmittingBlockstates() {
-        // modId -> list of blockIds -> list of blockStates
-        HashMap<
-                String, // modId
-                HashMap<
-                        String, // blockId
-                        HashMap<
-                                Byte, // light level
-                                HashSet<
-                                        HashMap<Property<? extends Comparable<?>>, Comparable<?>>
-                                        >
-                                >
-                        >
-                > lightEmittingBlocks = new HashMap<>();
+    public static Map<String, Map<String, Map<Byte, Set<Map<Property, Comparable>>>>> getLightEmittingBlockstates() {
+        // modId -> blockIds -> light levels -> property value pairs
+        Map<String, Map<String, Map<Byte, Set<Map<Property, Comparable>>>>> lightEmittingBlocks = new HashMap<>();
 
         for (Block block : Registries.BLOCK) {
-            HashMap<Byte, HashSet<HashMap<Property<? extends Comparable<?>>, Comparable<?>>>> lightEmittingProperties = new HashMap<>();
+            Map<Byte, Set<Map<Property, Comparable>>> lightEmittingProperties = new HashMap<>();
 
             BlockState blockState = block.getDefaultState();
 
@@ -40,7 +29,7 @@ public class LightBlocksExtractor {
             for (Map<Property<?>, Comparable<?>> combination : combinations) {
                 BlockState testState = blockState;
 
-                HashMap<Property<? extends Comparable<?>>, Comparable<?>> propertyValuePairs = new HashMap<>();
+                HashMap<Property, Comparable> propertyValuePairs = new HashMap<>();
 
                 for (Map.Entry<Property<?>, Comparable<?>> entry : combination.entrySet()) {
                     Property property = entry.getKey();
@@ -61,42 +50,7 @@ public class LightBlocksExtractor {
             if (!lightEmittingProperties.isEmpty()) {
 
                 // Compress the propertySets
-                // If for the current light level, a single property goes through all possible values while the others are the same, remove them
-
-                HashMap<Byte, HashSet<HashMap<Property<? extends Comparable<?>>, Comparable<?>>>> compressedLightEmittingProperties = new HashMap<>();
-
-                for (Map.Entry<Byte, HashSet<HashMap<Property<? extends Comparable<?>>, Comparable<?>>>> entry : lightEmittingProperties.entrySet()) {
-                    byte lightLevel = entry.getKey();
-                    compressedLightEmittingProperties.putIfAbsent(lightLevel, new HashSet<>());
-
-                    Map<Property, Set<Comparable>> usedValues = new HashMap<>();
-
-                    for (HashMap<Property<? extends Comparable<?>>, Comparable<?>> propertySet : entry.getValue()) {
-                        for (Map.Entry<Property<? extends Comparable<?>>, Comparable<?>> propertyValuePair : propertySet.entrySet()) {
-                            Property property = propertyValuePair.getKey();
-                            Comparable value = propertyValuePair.getValue();
-
-                            usedValues.computeIfAbsent(property, k -> new HashSet<>()).add(value);
-                        }
-                    }
-
-                    for (HashMap<Property<? extends Comparable<?>>, Comparable<?>> propertySet : entry.getValue()) {
-                        HashMap<Property<? extends Comparable<?>>, Comparable<?>> compressedPropertySet = new HashMap<>();
-
-                        for (Map.Entry<Property<? extends Comparable<?>>, Comparable<?>> propertyValuePair : propertySet.entrySet()) {
-                            Property property = propertyValuePair.getKey();
-                            Comparable value = propertyValuePair.getValue();
-
-                            if (property.getValues().size() != usedValues.get(property).size()) {
-                                compressedPropertySet.put(property, value);
-                            }
-                        }
-
-                        compressedLightEmittingProperties.get(lightLevel).add(compressedPropertySet);
-                    }
-                }
-
-                lightEmittingProperties = compressedLightEmittingProperties;
+                lightEmittingProperties = compressLightEmittingProperties(lightEmittingProperties);
 
                 String[] parts = block.getTranslationKey().split("\\.");
                 String modId = parts[1];
@@ -109,15 +63,15 @@ public class LightBlocksExtractor {
 
         LOGGER.info("Light emitting blocks:");
         LOGGER.info("Mod count: {}", lightEmittingBlocks.size());
-        for (Map.Entry<String, HashMap<String, HashMap<Byte, HashSet<HashMap<Property<? extends Comparable<?>>, Comparable<?>>>>>> entry : lightEmittingBlocks.entrySet()) {
+        for (Map.Entry<String, Map<String, Map<Byte, Set<Map<Property, Comparable>>>>> entry : lightEmittingBlocks.entrySet()) {
             LOGGER.info("Mod: {}; With {} light emitting blocks:", entry.getKey(), entry.getValue().size());
-            for (Map.Entry<String, HashMap<Byte, HashSet<HashMap<Property<? extends Comparable<?>>, Comparable<?>>>>> entry2 : entry.getValue().entrySet()) {
+            for (Map.Entry<String, Map<Byte, Set<Map<Property, Comparable>>>> entry2 : entry.getValue().entrySet()) {
                 LOGGER.info("  - Blockstate: {}; With {} light levels:", entry2.getKey(), entry2.getValue().size());
-                for (Map.Entry<Byte, HashSet<HashMap<Property<? extends Comparable<?>>, Comparable<?>>>> entry3 : entry2.getValue().entrySet()) {
+                for (Map.Entry<Byte, Set<Map<Property, Comparable>>> entry3 : entry2.getValue().entrySet()) {
                     LOGGER.info("    - Light level: {}; With {} property sets:", entry3.getKey(), entry3.getValue().size());
-                    for (HashMap<Property<? extends Comparable<?>>, ?> propertySet : entry3.getValue()) {
+                    for (Map<Property, Comparable> propertySet : entry3.getValue()) {
                         LOGGER.info("      - Property count: {}", propertySet.size());
-                        for (Map.Entry<Property<? extends Comparable<?>>, ?> propertyValuePair : propertySet.entrySet()) {
+                        for (Map.Entry<Property, Comparable> propertyValuePair : propertySet.entrySet()) {
                             LOGGER.info("        - {}={}", propertyValuePair.getKey().getName(), propertyValuePair.getValue());
                         }
                     }
@@ -128,13 +82,40 @@ public class LightBlocksExtractor {
         return lightEmittingBlocks;
     }
 
-    public static HashMap<Byte, HashSet<HashMap<Property<? extends Comparable<?>>, ?>>> compressLightEmittingProperties() {
-        HashMap<
-                Byte, // light level
-                HashSet<
-                        HashMap<Property<? extends Comparable<?>>, ?>
-                        >
-                > compressedLightEmittingProperties = new HashMap<>();
+    @SuppressWarnings("rawtypes")
+    public static Map<Byte, Set<Map<Property, Comparable>>> compressLightEmittingProperties(Map<Byte, Set<Map<Property, Comparable>>> lightEmittingProperties) {
+        Map<Byte, Set<Map<Property, Comparable>>> compressedLightEmittingProperties = new HashMap<>();
+
+        for (Map.Entry<Byte, Set<Map<Property, Comparable>>> entry : lightEmittingProperties.entrySet()) {
+            byte lightLevel = entry.getKey();
+            compressedLightEmittingProperties.putIfAbsent(lightLevel, new HashSet<>());
+
+            Map<Property, Set<Comparable>> usedValues = new HashMap<>();
+
+            for (Map<Property, Comparable> propertySet : entry.getValue()) {
+                for (Map.Entry<Property, Comparable> propertyValuePair : propertySet.entrySet()) {
+                    Property property = propertyValuePair.getKey();
+                    Comparable value = propertyValuePair.getValue();
+
+                    usedValues.computeIfAbsent(property, k -> new HashSet<>()).add(value);
+                }
+            }
+
+            for (Map<Property, Comparable> propertySet : entry.getValue()) {
+                Map<Property, Comparable> compressedPropertySet = new HashMap<>();
+
+                for (Map.Entry<Property, Comparable> propertyValuePair : propertySet.entrySet()) {
+                    Property property = propertyValuePair.getKey();
+                    Comparable value = propertyValuePair.getValue();
+
+                    if (property.getValues().size() != usedValues.get(property).size()) {
+                        compressedPropertySet.put(property, value);
+                    }
+                }
+
+                compressedLightEmittingProperties.get(lightLevel).add(compressedPropertySet);
+            }
+        }
 
         return compressedLightEmittingProperties;
     }
