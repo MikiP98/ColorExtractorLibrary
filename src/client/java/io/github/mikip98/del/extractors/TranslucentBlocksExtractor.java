17700ed92b265409056418d73286c02548ced971
MikiP98 <pokora.mikolaj@gmail.com>
2025-01-15 18:53:57 +0100

    Small refactor and code improvements

--- src/client/java/io/github/mikip98/del/extractors/TranslucentBlocksExtractor.java
+++ src/client/java/io/github/mikip98/del/extractors/TranslucentBlocksExtractor.java
@@ -1,6 +1,7 @@
 package io.github.mikip98.del.extractors;
 
 import io.github.mikip98.del.util.Util;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.Map;
@@ -8,7 +9,7 @@ import java.util.Map;
 
 public class TranslucentBlocksExtractor {
 
-    public static Map<String, List<String>> getTranslucentBlocks() {
+    public static @NotNull Map<String, List<String>> getTranslucentBlocks() {
         return Util.renderLayerModBlockMap.get("translucent");
     }
 }
